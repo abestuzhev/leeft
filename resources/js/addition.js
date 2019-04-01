550a04b29b36d0bbea7017efc4af1af1f4ce378f
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2019-04-01 07:35:36 +0300

    changes

--- resources/js/addition.js
+++ resources/js/addition.js
@@ -68,6 +68,37 @@ $(function(){
         ]
     });
 
+    $('.bitrix-keys-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        arrows: true,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
     $('.teh-example-slider').slick({
         infinite: true,
         slidesToShow: 4,
