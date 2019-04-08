c330b4772a21ebb2dabfb64452c05ea5b0f54590
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2019-04-08 07:46:44 +0300

    changes

--- resources/js/addition.js
+++ resources/js/addition.js
@@ -75,22 +75,14 @@ $(function(){
         slidesToScroll: 1,
         responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-
-            },
-            {
-                breakpoint: 640,
+                breakpoint: 920,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 640,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
